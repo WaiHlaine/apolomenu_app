edbb15dc7e4314bca304a2196386b6ad5389ac12
Naing Naing Htun <naingnainghtun662@gmail.com>
2025-11-15 17:45:20 +0630

    fix(order): removed duplicated routes

--- routes/branch_menu.php
+++ routes/branch_menu.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\BranchMenuController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{tenant_id}/{branch_id}/{table_public_token}/menu_items', [BranchMenuController::class, 'branchMenus'])->name('branch_menu.index');
+// Route::get('/{tenant_id}/{branch_id}/{table_public_token}/menu_items', [BranchMenuController::class, 'branchMenus'])->name('branch_menu.index');
 Route::get('/{tenant_id}/{branch_id}/{table_public_token}/menu_items/filter', [BranchMenuController::class, 'filterMenuItems'])->name('menu_item.filter');
 Route::get('/{tenant_id}/{branch_id}/{table_public_token}/menu_items/{id}', [BranchMenuController::class, 'showMenu'])->name('menu_item.show');
