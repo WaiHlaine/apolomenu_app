5028f57df24877b1dfe2ad58aae3b4064c87d3d9
Naing Naing Htun <naingnainghtun662@gmail.com>
2025-11-15 17:42:01 +0630

    fix(order): removed duplicate route

--- routes/order.php
+++ routes/order.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\BranchMenuController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{tenant_id}/{branch_id}/{table_public_token}/menus', [BranchMenuController::class, 'branchMenus'])->name('branch_menu.index');
+// Route::get('/{tenant_id}/{branch_id}/{table_public_token}/menus', [BranchMenuController::class, 'branchMenus'])->name('branch_menu.index');
 Route::get('/order/{order_number}', [OrderController::class, 'show'])->name('order.show');
 Route::get('/{tenant_id}/{branch_id}/{table_public_token}/orders', [OrderController::class, 'getActiveOrdersByTable'])->name('order.active');
 
